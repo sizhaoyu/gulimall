8834ca89bae5726c8dc4b97640fc31195b01f6ed
sizhaoyu <798726104@qq.com>
2021-06-01 08:46:34 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/tuskasa/guimall/member/GuimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuskasa.guimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
